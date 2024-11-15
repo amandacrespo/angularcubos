00172dc861409356d36b2417091de7a92dbf6514
amandacrespo <mancreslu@hotmail.com>
2024-11-15 11:52:23 +0100

    examen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/',
+    component: HomeComponent
+  },
+  // {
+  //     path: '/cubo/:id',
+  //     component: CuboComponent
+  // },
+  // {
+  //     path: '/cubos/:marca',
+  //     component: CubosMarca
+  // },
+// {
+//     path: '/login',
+//     component: LoginComponent
+// },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    urlCubos: 'https://apitiendacubos2024a.azurewebsites.net/',
+};
