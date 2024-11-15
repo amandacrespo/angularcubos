65df1b5d6427db7e1650902d00d7ba23db2e6ba1
amandacrespo <mancreslu@hotmail.com>
2024-11-15 13:02:23 +0100

    examen

--- src/app/components/cubosmarca/cubosmarca.component.ts
+++ src/app/components/cubosmarca/cubosmarca.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cubo } from '../../models/Cubo';
+import { CubosService } from '../../services/cubos.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cubosmarca',
   templateUrl: './cubosmarca.component.html',
   styleUrl: './cubosmarca.component.css'
 })
-export class CubosmarcaComponent {
+export class CubosmarcaComponent implements OnInit{
+  public cubos!: Cubo[];
+  public marca!: string;
 
+  constructor(public _service: CubosService, public _router:Router, public _activeRoute: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this._activeRoute.params.subscribe((params: Params)=>{
+      this.loadData(params['marca']);
+      this.marca = params['marca'];
+    })
+  }
+
+  loadData(marca:string):void {
+    this._service.getCubosMarca(marca)
+    .then(res => {
+        this.cubos = res;
+    })
+  }
 }
